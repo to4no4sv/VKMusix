e768ae11117e8af049dafe20e8b097597614167b
to4no4sv <104466820+to4no4sv@users.noreply.github.com>
2024-08-30 01:20:36 +0300

    Version 3.3.6

--- version.py
+++ version.py
@@ -16,4 +16,4 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with VKMusix. If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "3.3.5"
+__version__ = "3.3.6"

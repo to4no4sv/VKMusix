30244f84dbf616448201f43ef1be4285dadb355d
to4no4sv <104466820+to4no4sv@users.noreply.github.com>
2024-10-09 22:28:13 +0300

    Version 4.0.0

--- cookies.py
+++ cookies.py
@@ -29,7 +29,6 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
-
 def createOptions() -> Options:
     options = Options()
 
